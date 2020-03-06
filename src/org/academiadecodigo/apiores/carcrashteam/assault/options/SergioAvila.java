6f46a87c484df30bd0068d5ff826f51f11e6e500
claudiapinto <claudiaa.pinto.5@gmail.com>
2020-03-05 23:50:06 -0100

    created necessary classes in assault package | interface, factory and OldLady class finished

---
+++ src/org/academiadecodigo/apiores/carcrashteam/assault/options/SergioAvila.java
@@ -0,0 +1,20 @@
+package org.academiadecodigo.apiores.carcrashteam.assault.options;
+
+import org.academiadecodigo.apiores.carcrashteam.assault.Assault;
+
+public class SergioAvila implements Assault {
+    @Override
+    public int successRate() {
+        return 0;
+    }
+
+    @Override
+    public int weaponsRate() {
+        return 0;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
