b44cbdcb81b354da01587f9916ebca2fea1a8436
claudiapinto <claudiaa.pinto.5@gmail.com>
2020-03-08 14:42:07 -0100

    minor corrections

--- gtaterceiramaven/src/main/java/carcrashteam/utilities/Messages.java
+++ gtaterceiramaven/src/main/java/carcrashteam/utilities/Messages.java
@@ -10,29 +10,29 @@ public class Messages {
     public static final String SENT_PRISON = "You were sent to prison. Now you have to wait to be released.\n Waiting time: ";
     public static final String SUCCESS_ASSAULT = "Success! You manage to assault: ";
 
-    public static final String GO_DELMAN = "Get Drunk at Delman";
-    public static final String DELMAN_WELCOME = "Welcome to Delman's.\nThe entry cost will be 30$.";
-    public static final String DELMAN_NO_LUCK_MESSAGE = "You got wasted and the Special Chicken toast was rotten! You were sent to the hospital!";
-    public static final String DELMAN_NO_LUCK_STATUS = "You lost 15 Experience Points, you now have ";
-    public static final String DELMAN_LUCK = "Gained 25 Energy Points, you now have ";
+    public static final String GO_DELMAN = "\nGet Drunk at Delman";
+    public static final String DELMAN_WELCOME = "\nWelcome to Delman's.\nThe entry cost will be 30$.";
+    public static final String DELMAN_NO_LUCK_MESSAGE = "\nYou got wasted and the Special Chicken toast was rotten! You were sent to the hospital!";
+    public static final String DELMAN_NO_LUCK_STATUS = "\nYou lost 15 Experience Points, you now have ";
+    public static final String DELMAN_LUCK = "\nGained 25 Energy Points, you now have ";
 
     public static final String GO_CLASSIC_BAR = "Find some bitJS at Classic Bar.";
-    public static final String CLASSIC_BAR_WELCOME ="Welcome to Classic Bar! Get laid or die trying!\nYour entry cost will be 80$";
-    public static final String CLASSIC_BAR_NO_LUCK = "That hooker was dirty as fuck! You got AIDS and you were sent to the Hospital!";
+    public static final String CLASSIC_BAR_WELCOME ="\nWelcome to Classic Bar! Get laid or die trying!\nYour entry cost will be 80$";
+    public static final String CLASSIC_BAR_NO_LUCK = "\nThat hooker was dirty as fuck! You got AIDS and you were sent to the Hospital!";
     public static final String CLASSIC_BAR_LUCK = "So good to get laid! Energy Points gained: ";
 
-    public static final String BUY_WEED ="Get some Weed In Santa Rita Guetto!";
-    public static final String WEED_WELCOME = "Welcome to Santa Rita guetto! We have the best Weed for you sir!\nThe cost will be 60$";
-    public static final String WEED_NO_LUCK = "You got a bad trip and you were sent to the Hospital!";
-    public static final String WEED_LUCK = "Woot woot! Nice blunt! I'm Fliyng! Gained 50 Energy Points.\nYour Energy Points are now: ";
+    public static final String BUY_WEED ="\nGet some Weed In Santa Rita Guetto!";
+    public static final String WEED_WELCOME = "\nWelcome to Santa Rita guetto! We have the best Weed for you sir!\nThe cost will be 60$";
+    public static final String WEED_NO_LUCK = "\nYou got a bad trip and you were sent to the Hospital!";
+    public static final String WEED_LUCK = "\nWoot woot! Nice blunt! I'm Fliyng! Gained 50 Energy Points.\nYour Energy Points are now: ";
 
     public static final String RUBBER_DUCK_DESCRIPTION = "Rubber Duck";
     public static final String KNIFE_DESCRIPTION = "Knife";
     public static final String SHOTGUN_DESCRIPTION = "Shotgun";
     public static final String LIGHT_SABER_DESCRIPTION = "Light Saber";
 
-    public static final String ENTERING_HOSPITAL = "Welcome to Praia da Vitoria Health Center! Here we will take good care of your wounds!";
-    public static final String LEAVING_HOSPITAL = "Your're healed, charged you 200$! Good luck avoid COVID-19!";
+    public static final String ENTERING_HOSPITAL = "\nWelcome to Praia da Vitoria Health Center! Here we will take good care of your wounds!";
+    public static final String LEAVING_HOSPITAL = "\nYour're healed, charged you 200$! Good luck avoid COVID-19!";
 
     public static final String MAIN_MENU_RULE = "\n" +
             "                   _       __     __                             __           _______________       ____________________  __     _                            \n" +
@@ -43,11 +43,10 @@ public class Messages {
             "                                                                                                                                                              \n" +
             "You will have the chance to commit robbery to important targets, go out to some great places or attack other players, you chose.\n" +
             "Insert your nickname:\n";
-
-    public static final String ROBBERY_RULE ="In case of success your Experience Points will increase and you might get a WEAPON! \n It will cost you Energy Points.\n Be careful thought, you might end up in Prison.";
-    public static final String NIGHTLIFE_RULE = "If you're lucky, your Energy Points will increase. \n Otherwise, your Experience Points will decrement and you will end up in the Hospital healing.";
-    //public static final String HOSPITAL_RULE = "Your Health Points will be restored. It will cost you 200 $";
-    public static final String CONTINENTE_RULE = "Buy the WEAPONS you were not able to win during the Robbery.";
-    public static final String STATUS_RULE = "Here is your information:";
-    public static final String NOT_ENOUGH_MONEY = "You dont't have enough money tto buy this item";
+    public static final String EXISTING_NAME ="\nThis name is already taken. Please choose a different one.";
+    public static final String ROBBERY_RULE ="\nYou will gain Experience Points for each robbery but it will cost you Energy Points.\nIn case of success you will earn money and if you're lucky you might win a weapon for your arsenal!\nBe careful thought, you might end up in prison.";
+    public static final String NIGHTLIFE_RULE = "\nAll have an entry cost.\nIf you're lucky, your Energy Points will increase.\nOtherwise, your Experience Points will decrement and you will end up in the hospital.";
+    public static final String CONTINENTE_RULE = "\nBuy the weapons you were not able to win during the robbery.\nCheck if you have enough money though.";
+    public static final String STATUS_RULE = "\nHere is your information:";
+    public static final String NOT_ENOUGH_MONEY = "\nYou dont't have enough money to buy this item.";
 }
